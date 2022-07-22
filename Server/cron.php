0bc33e5b8bca7405fc4560152500acf8d95da3b3
iBerq <ayberk.yilmaz@icloud.com>
2022-07-23 02:04:20 +0300

    Bug fixes

--- Server/cron.php
+++ Server/cron.php
@@ -1,15 +1,19 @@
 <?php
 include 'database.php';
 parse_str(implode('&', array_slice($argv, 1)), $_GET);
-$cron_id = $_GET["cron_id"];
+$cron_id = $_GET["id"];
 
-$cron_job = get_object_vars(select_where(["*"], "cron_jobs", ["cron_id" => $cron_id])[0]);
+$cron_job = get_object_vars(select_where(["*"], "cron_jobs", ["id" => $cron_id])[0]);
 
 $values = array(
     "command" => $cron_job["command"],
     "runned_by" => "cron",
 );
-$post_url = "http://localhost/command/exec/" . $cron_job["machine_name"];
+
+$post_url = "http://localhost/command/exec";
+if ($cron_job["machine_name"] != "*")
+    $post_url .= "/" . $cron_job["machine_name"];
+
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $post_url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -19,6 +23,4 @@ curl_setopt($ch, CURLOPT_FRESH_CONNECT, true);
 curl_setopt($ch, CURLOPT_TIMEOUT, 20);
 $resultJSON = @curl_exec($ch);
 $result = json_decode($resultJSON, true);
-$command = $result["command"][0]["command_id"];
-echo $command;
-update("cron_jobs", ["last_runned_command_id" => $result["command"][0]["command_id"]], ["cron_id" => $cron_id]);
+update("cron_jobs", ["last_runned_command_id" => ($cron_job["machine_name"] == "*") ? $result["result"][0][0]["id"] : $result["result"][0]["id"]], ["id" => $cron_id]);
